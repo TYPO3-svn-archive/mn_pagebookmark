4327c0bcc7133a28cee3fed193e159ae46bf07c4
super-mario <super-mario@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-24 22:22:31 +0000

    - update to version 1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mn_pagebookmark/trunk@30465 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "mn_pagebookmark".
 #
-# Auto generated 03-02-2010 22:29
+# Auto generated 24-02-2010 23:15
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,8 +15,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'List of Bookmarks',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.1.1',
-	'dependencies' => '',
+	'version' => '1.0.0',
+	'dependencies' => 'cms',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -41,7 +41,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:27:{s:9:"ChangeLog";s:4:"f995";s:10:"README.txt";s:4:"b89c";s:10:"backup.php";s:4:"f999";s:12:"ext_icon.gif";s:4:"af60";s:17:"ext_localconf.php";s:4:"74ea";s:14:"ext_tables.php";s:4:"ce48";s:14:"ext_tables.sql";s:4:"a1ca";s:35:"icon_tx_mnpagebookmark_bookmark.gif";s:4:"78eb";s:16:"locallang_db.xml";s:4:"79b2";s:7:"tca.php";s:4:"e15c";s:19:"doc/wizard_form.dat";s:4:"cea4";s:20:"doc/wizard_form.html";s:4:"df83";s:32:"hooks/class.tx_bookmark_hook.php";s:4:"a7b9";s:40:"lib/class.tx_mnpagebookmark_bookmark.php";s:4:"3775";s:44:"lib/class.tx_mnpagebookmark_bookmarklist.php";s:4:"e459";s:42:"lib/class.tx_mnpagebookmark_controller.php";s:4:"d2d1";s:39:"lib/class.tx_mnpagebookmark_display.php";s:4:"4d1e";s:35:"lib/class.tx_mnpagebookmark_div.php";s:4:"b53b";s:51:"lib/class.tx_mnpagebookmark_parametercontroller.php";s:4:"4273";s:35:"pi1/class.tx_mnpagebookmark_pi1.php";s:4:"8983";s:17:"pi1/locallang.xml";s:4:"00a6";s:20:"pi1/static/setup.txt";s:4:"d41d";s:25:"res/merkzettel_short.html";s:4:"1e3e";s:17:"res/template.html";s:4:"a37c";s:31:"res/images/gtk-delete_24x24.png";s:4:"8ed1";s:20:"static/constants.txt";s:4:"d41d";s:16:"static/setup.txt";s:4:"5f71";}',
+	'_md5_values_when_last_written' => 'a:28:{s:9:"ChangeLog";s:4:"f995";s:10:"README.txt";s:4:"7d61";s:10:"backup.php";s:4:"1894";s:12:"ext_icon.gif";s:4:"af60";s:17:"ext_localconf.php";s:4:"99d7";s:14:"ext_tables.php";s:4:"9586";s:14:"ext_tables.sql";s:4:"a1ca";s:35:"icon_tx_mnpagebookmark_bookmark.gif";s:4:"78eb";s:16:"locallang_db.xml";s:4:"64de";s:7:"tca.php";s:4:"94f4";s:14:"doc/manual.sxw";s:4:"72c7";s:19:"doc/wizard_form.dat";s:4:"cea4";s:20:"doc/wizard_form.html";s:4:"356e";s:32:"hooks/class.tx_bookmark_hook.php";s:4:"db80";s:40:"lib/class.tx_mnpagebookmark_bookmark.php";s:4:"f9e9";s:44:"lib/class.tx_mnpagebookmark_bookmarklist.php";s:4:"3c0a";s:42:"lib/class.tx_mnpagebookmark_controller.php";s:4:"7338";s:39:"lib/class.tx_mnpagebookmark_display.php";s:4:"0a95";s:35:"lib/class.tx_mnpagebookmark_div.php";s:4:"5898";s:51:"lib/class.tx_mnpagebookmark_parametercontroller.php";s:4:"e600";s:35:"pi1/class.tx_mnpagebookmark_pi1.php";s:4:"0445";s:17:"pi1/locallang.xml";s:4:"a26f";s:20:"pi1/static/setup.txt";s:4:"d41d";s:25:"res/merkzettel_short.html";s:4:"c1bf";s:17:"res/template.html";s:4:"113d";s:31:"res/images/gtk-delete_24x24.png";s:4:"8ed1";s:20:"static/constants.txt";s:4:"898b";s:16:"static/setup.txt";s:4:"9649";}',
+	'suggests' => array(
+	),
 );
 
 ?>
